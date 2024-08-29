5bbee1fc10cc66bd2513998ab0208b2a52e8fde1
Santiago Cecena <santycc00@hotmail.com>
2024-08-29 13:32:16 -0700

    feat: add functionality and styles
    Create how the change figure functionality will work

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,38 @@
+import { useState } from 'react'
 import cs from './Card.module.css'
 import { Triangle } from './Triangle'
+import { Rectangle } from './Rectangle';
+import { Circle } from './Circle';
 
+enum figures {
+    triangle = 'triangle',
+    rectangle = 'rectangle',
+    circle = 'circle',
+}
 
 export function Card() {
-    
+
+    const [figure, setFigure] = useState<figures>(figures.triangle)
+
+    let FigureComponent = Triangle;
+    if (figure === figures.rectangle) {
+        FigureComponent = Rectangle
+    } else if (figure === figures.circle) {
+        FigureComponent = Circle
+    } else {
+        FigureComponent = Triangle;
+    }
+
     return (
         <section className={cs.card}>
             <h1>Calculadora area y perimetro</h1>
-            <Triangle />
+            <div className={cs.btnDiv}>
+                <button className={cs.button} onClick={() => setFigure(figures.triangle)}>Triangle</button>
+                <button className={cs.button} onClick={() => setFigure(figures.rectangle)}>Rectangle</button>
+                <button className={cs.button} onClick={() => setFigure(figures.circle)}>Circle</button>
+            </div>
+
+            <FigureComponent />
         </section>
     )
 }
